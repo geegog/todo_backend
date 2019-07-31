341cb8845d5932bc2a4d5575e82800e230384b69
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-07-31 22:36:00 +0300

    All repo Test
    passed

--- test/todo_backend/category/repository/todo_category_test.exs
+++ test/todo_backend/category/repository/todo_category_test.exs
@@ -2,6 +2,9 @@ defmodule TodoBackend.Category.Repository.TodoCategoryTest do
   use TodoBackend.DataCase
 
   alias TodoBackend.Category.Repository.TodoCategoryRepo
+  alias TodoBackend.Category.Repository.CategoryRepo
+  alias TodoBackend.Task.Repository.TodoRepo
+  alias TodoBackend.User.Repository.UserRepo
 
   describe "todo_categories" do
     alias TodoBackend.Category.Model.TodoCategory
@@ -9,28 +12,64 @@ defmodule TodoBackend.Category.Repository.TodoCategoryTest do
     @valid_attrs %{}
     @update_attrs %{}
     @invalid_attrs %{}
+    @valid_user_attrs %{email: "some@email.com", name: "some name", phone: "123456789", password: "password", password_confirmation: "password"}
+    @valid_todo_attrs %{deadline: ~N[2010-04-17 14:00:00], description: "some description", title: "some title"}
+    @valid_category_attrs %{name: "some name"}
 
     def todo_category_fixture(attrs \\ %{}) do
+      category = category_fixture()
+      todo = todo_fixture()
+
       {:ok, todo_category} =
         attrs
-        |> Enum.into(@valid_attrs)
+        |> Enum.into(@valid_attrs |> Map.put(:todo_id, todo.id) |> Map.put(:category_id, category.id))
         |> TodoCategoryRepo.create_todo_category()
 
       todo_category
     end
 
+    def category_fixture(attrs \\ %{}) do
+      {:ok, category} =
+        attrs
+        |> Enum.into(@valid_category_attrs)
+        |> CategoryRepo.create_category()
+
+      category
+    end
+
+    def todo_fixture(attrs \\ %{}) do
+      user = user_fixture()
+      {:ok, todo} =
+        attrs
+        |> Enum.into(@valid_todo_attrs |> Map.put(:user_id, user.id))
+        |> TodoRepo.create_todo()
+
+      todo |> TodoRepo.preload
+    end
+
+    def user_fixture(attrs \\ %{}) do
+      {:ok, user} =
+        attrs
+        |> Enum.into(@valid_user_attrs)
+        |> UserRepo.create_user()
+
+      user
+    end
+
     test "list_todo_categories/0 returns all todo_categories" do
-      todo_category = todo_category_fixture()
+      todo_category = todo_category_fixture() |> TodoCategoryRepo.preload
       assert TodoCategoryRepo.list_todo_categories() == [todo_category]
     end
 
     test "get_todo_category!/1 returns the todo_category with given id" do
-      todo_category = todo_category_fixture()
+      todo_category = todo_category_fixture() |> TodoCategoryRepo.preload
       assert TodoCategoryRepo.get_todo_category!(todo_category.id) == todo_category
     end
 
     test "create_todo_category/1 with valid data creates a todo_category" do
-      assert {:ok, %TodoCategory{} = todo_category} = TodoCategoryRepo.create_todo_category(@valid_attrs)
+      category = category_fixture()
+      todo = todo_fixture()
+      assert {:ok, %TodoCategory{} = todo_category} = TodoCategoryRepo.create_todo_category(@valid_attrs |> Map.put(:todo_id, todo.id) |> Map.put(:category_id, category.id))
     end
 
     test "create_todo_category/1 with invalid data returns error changeset" do
@@ -45,10 +84,10 @@ defmodule TodoBackend.Category.Repository.TodoCategoryTest do
     end
 
     test "update_todo_category/2 with invalid data returns error changeset" do
-      todo_category = todo_category_fixture()
+      todo_category = todo_category_fixture() |> TodoCategoryRepo.preload
 
       assert {:error, %Ecto.Changeset{}} =
-      TodoCategoryRepo.update_todo_category(todo_category, @invalid_attrs)
+      TodoCategoryRepo.update_todo_category(todo_category |> Map.drop([:category_id]), @invalid_attrs)
 
       assert todo_category == TodoCategoryRepo.get_todo_category!(todo_category.id)
     end
--- test/todo_backend/task/repository/comment_test.exs
+++ test/todo_backend/task/repository/comment_test.exs
@@ -5,32 +5,57 @@ defmodule TodoBackend.Task.Repository.CommentTest do
 
   describe "comments" do
     alias TodoBackend.Task.Model.Comment
+    alias TodoBackend.Task.Repository.TodoRepo
+    alias TodoBackend.User.Repository.UserRepo
 
     @valid_attrs %{text: "some text"}
     @update_attrs %{text: "some updated text"}
     @invalid_attrs %{text: nil}
+    @valid_user_attrs %{email: "some@email.com", name: "some name", phone: "123456789", password: "password", password_confirmation: "password"}
+    @valid_todo_attrs %{deadline: ~N[2010-04-17 14:00:00], description: "some description", title: "some title"}
 
     def comment_fixture(attrs \\ %{}) do
+      todo = todo_fixture()
       {:ok, comment} =
         attrs
-        |> Enum.into(@valid_attrs)
+        |> Enum.into(@valid_attrs |> Map.put(:user_id, todo.user.id) |> Map.put(:todo_id, todo.id))
         |> CommentRepo.create_comment()
 
       comment
     end
 
+    def todo_fixture(attrs \\ %{}) do
+      user = user_fixture()
+      {:ok, todo} =
+        attrs
+        |> Enum.into(@valid_todo_attrs |> Map.put(:user_id, user.id))
+        |> TodoRepo.create_todo()
+
+      todo |> TodoRepo.preload
+    end
+
+    def user_fixture(attrs \\ %{}) do
+      {:ok, user} =
+        attrs
+        |> Enum.into(@valid_user_attrs)
+        |> UserRepo.create_user()
+
+      user
+    end
+
     test "list_comments/0 returns all comments" do
       comment = comment_fixture()
-      assert CommentRepo.list_comments() == [comment]
+      assert length(CommentRepo.list_comments()) == length([comment])
     end
 
     test "get_comment!/1 returns the comment with given id" do
-      comment = comment_fixture()
+      comment = comment_fixture() |> CommentRepo.preload
       assert CommentRepo.get_comment!(comment.id) == comment
     end
 
     test "create_comment/1 with valid data creates a comment" do
-      assert {:ok, %Comment{} = comment} = CommentRepo.create_comment(@valid_attrs)
+      todo = todo_fixture()
+      assert {:ok, %Comment{} = comment} = CommentRepo.create_comment(@valid_attrs |> Map.put(:user_id, todo.user.id) |> Map.put(:todo_id, todo.id))
       assert comment.text == "some text"
     end
 
@@ -45,7 +70,7 @@ defmodule TodoBackend.Task.Repository.CommentTest do
     end
 
     test "update_comment/2 with invalid data returns error changeset" do
-      comment = comment_fixture()
+      comment = comment_fixture() |> CommentRepo.preload
       assert {:error, %Ecto.Changeset{}} = CommentRepo.update_comment(comment, @invalid_attrs)
       assert comment == CommentRepo.get_comment!(comment.id)
     end
