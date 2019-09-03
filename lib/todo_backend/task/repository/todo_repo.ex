f9d2e1b44bc5bf94670e6657c9c641fb24e2b3f5
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-09-03 21:43:14 +0300

    Sort order changed to desc

--- lib/todo_backend/task/repository/todo_repo.ex
+++ lib/todo_backend/task/repository/todo_repo.ex
@@ -18,7 +18,7 @@ defmodule TodoBackend.Task.Repository.TodoRepo do
 
   """
   def list_todos(params) do
-    query = from(t in Todo, preload: [:user], order_by: [asc: t.inserted_at, asc: t.id], select: t)
+    query = from(t in Todo, preload: [:user], order_by: [desc: t.inserted_at, desc: t.id], select: t)
 
     cond do
       params["after"] !== nil -> Repo.paginate(query, after: params["after"], cursor_fields: [:inserted_at, :id], limit: 10)
