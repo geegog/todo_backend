49e72964508057594626aa0f99477d038cc8faa5
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-09-23 00:57:11 +0300

    meta added to get todo comments

--- lib/todo_backend_web/views/comment_view.ex
+++ lib/todo_backend_web/views/comment_view.ex
@@ -3,7 +3,15 @@ defmodule TodoBackendWeb.CommentView do
   alias TodoBackendWeb.CommentView
 
   def render("index.json", %{comments: comments}) do
-    %{data: render_many(comments, CommentView, "comment.json")}
+    %{
+      data: render_many(comments, CommentView, "comment.json"),
+      metadata: %{
+        page_number: comments.page_number,
+        page_size: comments.page_size,
+        total_pages: comments.total_pages,
+        total_entries: comments.total_entries
+      }
+    }
   end
 
   def render("show.json", %{comment: comment}) do
@@ -11,10 +19,21 @@ defmodule TodoBackendWeb.CommentView do
   end
 
   def render("comment.json", %{comment: comment}) do
-    %{id: comment.id,
+    %{
+      id: comment.id,
       text: comment.text,
-      user: %{id: comment.user.id, email: comment.user.email, phone: comment.user.phone, name: comment.user.name},
-      todo: %{id: comment.todo.id, deadline: comment.todo.deadline, description: comment.todo.description, title: comment.todo.title}
+      user: %{
+        id: comment.user.id,
+        email: comment.user.email,
+        phone: comment.user.phone,
+        name: comment.user.name
+      },
+      todo: %{
+        id: comment.todo.id,
+        deadline: comment.todo.deadline,
+        description: comment.todo.description,
+        title: comment.todo.title
+      }
     }
   end
 end
--- lib/todo_backend_web/views/todo_view.ex
+++ lib/todo_backend_web/views/todo_view.ex
@@ -3,11 +3,15 @@ defmodule TodoBackendWeb.TodoView do
   alias TodoBackendWeb.TodoView
 
   def render("index.json", %{todos: todos}) do
-    %{data: render_many(todos.entries, TodoView, "todo.json"),
-    metadata: %{page_number: todos.page_number,
-    page_size: todos.page_size,
-    total_pages: todos.total_pages,
-    total_entries: todos.total_entries}}
+    %{
+      data: render_many(todos.entries, TodoView, "todo.json"),
+      metadata: %{
+        page_number: todos.page_number,
+        page_size: todos.page_size,
+        total_pages: todos.total_pages,
+        total_entries: todos.total_entries
+      }
+    }
   end
 
   def render("show.json", %{todo: todo}) do
@@ -20,7 +24,12 @@ defmodule TodoBackendWeb.TodoView do
       deadline: todo.deadline,
       description: todo.description,
       title: todo.title,
-      user: %{id: todo.user.id, email: todo.user.email, phone: todo.user.phone, name: todo.user.name}
+      user: %{
+        id: todo.user.id,
+        email: todo.user.email,
+        phone: todo.user.phone,
+        name: todo.user.name
+      }
     }
   end
 end
